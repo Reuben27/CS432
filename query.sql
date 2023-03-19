3673f109fb112ddf572754ceb8e91944ea6e96d8
Reuben27 <reuben.sd@iitgn.ac.in>
2023-03-19 14:44:55 +0530

    added query file

---
+++ query.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE dummy;
+
+USE dummy;
+
+CREATE TABLE practice(
+	ID int,
+    FirstName varchar(255),
+    Roll int
+);
+
+Insert into practice values(1, 'Reuben', 19110059);
+
+Select * from practice;
