e8bee00ce8c9df0937eb0d7e1b48cb2895abd434
Reuben27 <reuben.sd@iitgn.ac.in>
2023-03-20 23:28:19 +0200

    fix bugs and issues

--- website/__init__.py
+++ website/__init__.py
@@ -4,10 +4,18 @@ import MySQLdb.cursors
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import json
+import yaml
+
 
 lm = LoginManager()
 db = SQLAlchemy()
 mysql = MySQL()
+
+conf = yaml.safe_load(open('db.yaml'))
+sql_host = conf['mysql_host']
+sql_user = conf['mysql_user']
+sql_pass = conf['mysql_password']
+sql_db = conf['mysql_db']
 
 with open('website/tables.json', 'r') as f:
   tables_dict = json.load(f)
@@ -16,12 +24,14 @@ print(tables_dict)
 
 def create_app():
     app = Flask(__name__, template_folder='../templates',static_folder='../static')
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'reuben'
-    app.config['MYSQL_DB'] = 'sports_management'
-    app.config['SECRET_KEY'] = 'reuben'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:reuben@localhost/sports_management'
+    app.config['MYSQL_HOST'] = sql_host 
+    app.config['MYSQL_USER'] = sql_user
+    app.config['MYSQL_PASSWORD'] = sql_pass
+    app.config['MYSQL_DB'] = sql_db
+    app.config['SECRET_KEY'] = 'asdfdbds'
+
+    sqlalchemy_config = 'mysql://'+str(sql_user)+':'+str(sql_pass)+'@'+str(sql_host)+'/'+str(sql_db)
+    app.config['SQLALCHEMY_DATABASE_URI'] = sqlalchemy_config
     db.init_app(app)
     mysql.init_app(app)
 
