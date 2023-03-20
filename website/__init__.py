60841d67cadfb30fc3f6c2f8fecfe1b3067673c8
pallavj112 <85111363+pallavj112@users.noreply.github.com>
2023-03-20 16:25:16 +0530

    login logout signup working

--- website/__init__.py
+++ website/__init__.py
@@ -1,22 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_mysqldb import MySQL
 
 lm = LoginManager()
-
+db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='../templates')
     app.config['MYSQL_HOST'] = 'localhost'
     app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'Your Password'
-    app.config['MYSQL_DB'] = 'Your Database Name'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123@localhost/users'
-    db = SQLAlchemy(app)
-
-    mysql = MySQL(app)
+    app.config['MYSQL_PASSWORD'] = '123'
+    app.config['MYSQL_DB'] = 'sports_management'
+    app.config['SECRET_KEY'] = 'pallav'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123@localhost/sports_management'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -33,7 +31,7 @@ def create_app():
     lm.init_app(app)
 
     @lm.user_loader
-    def load_user(id):
-        return Users.query.get(int(id))
+    def load_user(user_id):
+        return Users.query.get(int(user_id))
 
     return app 
